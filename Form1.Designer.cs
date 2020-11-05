ab6a4c2b3f47656b72d418038166de5c2e5da5b9
Denis Rafi <62852838+DenisRafi@users.noreply.github.com>
2020-11-05 18:09:15 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,420 @@
+﻿namespace MakeYourOwnRetailApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.items = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.price = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.qty = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.total = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.sub = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dis = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.net = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.balance = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.paid = new System.Windows.Forms.TextBox();
+            this.rem = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 147);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // items
+            // 
+            this.items.FormattingEnabled = true;
+            this.items.Location = new System.Drawing.Point(247, 226);
+            this.items.Name = "items";
+            this.items.Size = new System.Drawing.Size(465, 21);
+            this.items.TabIndex = 1;
+            this.items.SelectedIndexChanged += new System.EventHandler(this.items_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(181, 226);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select Item";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(248, 184);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Chicken";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(346, 184);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(72, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Red Meat";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // price
+            // 
+            this.price.Location = new System.Drawing.Point(12, 293);
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(100, 20);
+            this.price.TabIndex = 5;
+            this.price.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 276);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Price";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(141, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Qty";
+            // 
+            // qty
+            // 
+            this.qty.Location = new System.Drawing.Point(141, 293);
+            this.qty.Name = "qty";
+            this.qty.Size = new System.Drawing.Size(100, 20);
+            this.qty.TabIndex = 7;
+            this.qty.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.qty.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 323);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Total";
+            // 
+            // total
+            // 
+            this.total.Location = new System.Drawing.Point(12, 339);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(100, 20);
+            this.total.TabIndex = 9;
+            this.total.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(718, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Add Item";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(247, 290);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(465, 248);
+            this.listView1.TabIndex = 12;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Item Name";
+            this.columnHeader1.Width = 250;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Price";
+            this.columnHeader2.Width = 75;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Qty";
+            this.columnHeader3.Width = 75;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Total";
+            this.columnHeader4.Width = 75;
+            // 
+            // sub
+            // 
+            this.sub.Location = new System.Drawing.Point(259, 561);
+            this.sub.Name = "sub";
+            this.sub.Size = new System.Drawing.Size(100, 20);
+            this.sub.TabIndex = 13;
+            this.sub.Text = "0";
+            this.sub.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(200, 568);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Sub Total";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(383, 568);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Discount";
+            // 
+            // dis
+            // 
+            this.dis.Location = new System.Drawing.Point(441, 561);
+            this.dis.Name = "dis";
+            this.dis.Size = new System.Drawing.Size(100, 20);
+            this.dis.TabIndex = 15;
+            this.dis.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.dis.TextChanged += new System.EventHandler(this.discount_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(557, 568);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Net";
+            // 
+            // net
+            // 
+            this.net.Location = new System.Drawing.Point(587, 561);
+            this.net.Name = "net";
+            this.net.Size = new System.Drawing.Size(100, 20);
+            this.net.TabIndex = 17;
+            this.net.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(383, 603);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 13);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Balance";
+            // 
+            // balance
+            // 
+            this.balance.Location = new System.Drawing.Point(441, 596);
+            this.balance.Name = "balance";
+            this.balance.Size = new System.Drawing.Size(100, 20);
+            this.balance.TabIndex = 21;
+            this.balance.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(225, 603);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Paid";
+            // 
+            // paid
+            // 
+            this.paid.Location = new System.Drawing.Point(259, 596);
+            this.paid.Name = "paid";
+            this.paid.Size = new System.Drawing.Size(100, 20);
+            this.paid.TabIndex = 23;
+            this.paid.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.paid.TextChanged += new System.EventHandler(this.paid_TextChanged);
+            // 
+            // rem
+            // 
+            this.rem.Location = new System.Drawing.Point(718, 319);
+            this.rem.Name = "rem";
+            this.rem.Size = new System.Drawing.Size(83, 23);
+            this.rem.TabIndex = 25;
+            this.rem.Text = "Remove Item";
+            this.rem.UseVisualStyleBackColor = true;
+            this.rem.Click += new System.EventHandler(this.rem_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(718, 348);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 23);
+            this.button2.TabIndex = 26;
+            this.button2.Text = "&Save";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(718, 377);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 23);
+            this.button3.TabIndex = 27;
+            this.button3.Text = "&Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(718, 406);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 23);
+            this.button4.TabIndex = 28;
+            this.button4.Text = "&Print";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 688);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.rem);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.paid);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.balance);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.net);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dis);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.sub);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.qty);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.price);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.items);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Make Your Own Retail App | ByDenisRafi";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox items;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox price;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox qty;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox total;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.TextBox sub;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox dis;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox net;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox balance;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox paid;
+        private System.Windows.Forms.Button rem;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MakeYourOwnRetailApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            items.Items.Clear();
+            items.Items.Add("Chicken Item 1");
+            items.Items.Add("Chicken Item 2");
+            items.Items.Add("Chicken Item 3");
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {           
+            
+            items.Items.Clear();
+            items.Items.Add("Red Meat 1");
+            items.Items.Add("Red Meat 2");
+            items.Items.Add("Red Meat 3");
+        }
+
+        private void items_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (items.SelectedItem.ToString() == "Chicken Item 1")
+            {
+                price.Text = "3";
+            }
+            else if (items.SelectedItem.ToString() == "Chicken Item 2")
+            {
+                price.Text = "5";
+            }
+            else if (items.SelectedItem.ToString() == "Chicken Item 3")
+            {
+                price.Text = "7";
+            }
+            else if (items.SelectedItem.ToString() == "Red Meat 1")
+            {
+                price.Text = "4";
+            }
+            else if (items.SelectedItem.ToString() == "Red Meat 2")
+            {
+                price.Text = "6";
+            }
+            else if (items.SelectedItem.ToString() == "Red Meat 3")
+            {
+                price.Text = "8";
+            }
+            else
+            {
+                price.Text = "0";
+            }
+
+            total.Text = "";   
+            qty.Text = "";  
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(qty.Text.Length > 0)
+            total.Text = (Convert.ToInt16(price.Text) * Convert.ToInt16(qty.Text)).ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] arr = new string[4];
+            arr[0] = items.SelectedItem.ToString();
+            arr[1] = price.Text;
+            arr[2] = qty.Text;
+            arr[3] = total.Text;
+
+            ListViewItem lvi = new ListViewItem(arr);
+            listView1.Items.Add(lvi);
+
+            sub.Text = (Convert.ToInt16(sub.Text) + Convert.ToInt16(total.Text)).ToString();
+        }
+
+        private void discount_TextChanged(object sender, EventArgs e)
+        {
+          if (dis.Text.Length > 0)
+          {
+                net.Text = (Convert.ToInt16(sub.Text) - Convert.ToInt16(dis.Text)).ToString();
+          }
+        }
+
+        private void paid_TextChanged(object sender, EventArgs e)
+        {
+            if (paid.Text.Length > 0)
+            {
+                balance.Text = (Convert.ToInt16(net.Text) - Convert.ToInt16(paid.Text)).ToString();
+            }
+        }
+
+        private void rem_Click(object sender, EventArgs e)
+        {
+           if (listView1.SelectedItems.Count > 0)
+           {
+             for (int i=0; i < listView1.Items.Count; i++)
+             {
+                    if (listView1.Items[i].Selected)
+                    {
+                        sub.Text = (Convert.ToInt16(sub.Text) - Convert.ToInt16(listView1.Items[i].SubItems[3].Text)).ToString();
+                        listView1.Items[i].Remove();
+                    }
+             } 
+           }
+        }
+    }
+}
